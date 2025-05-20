960a264937682e5ef132b7dbe68ec22d65ac775a
pikaboo24 <manipikaboo@gmail.com>
2025-05-20 19:52:32 -0400

    almost done with step 8

--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -1,5 +1,47 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Boid : MonoBehaviour
 {
+    
+    GameObject targetObject;
+    public Rigidbody rigidBody;
+    public float speedMax = 2;
+    public float accelMax = 3;
+   
+    private void Start()
+    {
+        targetObject = GameObject.Find("target");
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        //Get displacement vector to target
+        Vector3 toTarget = targetObject.transform.position - transform.position;  
+
+        //Normalize
+        Vector3 toTargetNormalized  =toTarget.normalized;
+
+        //Determine acceleration
+        Vector3 acceleration = toTargetNormalized * accelMax;
+
+
+        rigidBody.linearVelocity += acceleration * Time.fixedDeltaTime;
+
+        //Enforce a top speed
+        rigidBody.linearVelocity = Vector3.ClampMagnitude(rigidBody.linearVelocity, speedMax);
+
+
+        //Orient toward velocity
+        transform.forward = rigidBody.linearVelocity;
+
+    }
+
+
+
+
+
+
 }
--- Assets/BoidSimulation.cs
+++ Assets/BoidSimulation.cs
@@ -1,6 +1,59 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BoidSimulationControl : MonoBehaviour
 {
+    public GameObject BoidsPrefab = null;
+    public int numboidToSpawn = 10;
+    public List<Boid> boids = null;
+    public enum ControlMode
+    {
+        Seek,
+        Pursue,
+        Food,
+        Obstacle
+    }
+    private void Start()
+    {
+     
+        for (int i = 0; i <= numboidToSpawn; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(-1.4f, 1.4f), Random.Range(0, 1.4f), Random.Range(-0.9f, 0.9f));
+            Quaternion rotation = Random.rotation;
 
-}
+            GameObject spawnedBoid = Instantiate(BoidsPrefab, position, rotation);
+            boids.Add(spawnedBoid.GetComponent<Boid>());
+            spawnedBoid.GetComponent<Renderer>().material.SetColor("_BaseColor", Random.ColorHSV(0, 1, 0f, 1f, 0.5f, 1f));
+            spawnedBoid.GetComponent<Rigidbody>().linearVelocity = Random.onUnitSphere * 0.3f;
+
+            spawnedBoid.transform.localScale *= Random.Range(0.9f, 3f);
+        }
+
+    }
+
+
+
+    public ControlMode controlMode = ControlMode.Seek;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            controlMode = ControlMode.Seek;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            controlMode = ControlMode.Pursue;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            controlMode = ControlMode.Food;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            controlMode = ControlMode.Obstacle;
+        }
+    }
+
+}   
